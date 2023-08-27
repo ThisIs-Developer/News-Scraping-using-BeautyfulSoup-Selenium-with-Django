55418678640a70de744c72bce5f88ac48747ecb3
Baivab Sarkar <109382325+ThisIs-Developer@users.noreply.github.com>
2023-08-27 21:14:00 +0530

    Update models.py

--- scrap_app/models.py
+++ scrap_app/models.py
@@ -4,7 +4,7 @@ class HindustanTimesBangla(models.Model):
     headline = models.TextField()
     sort_description = models.TextField()
     news = models.TextField()
-    image_source = models.URLField()
+    image_source = models.URLField(max_length=1000)  # Increase max_length if needed
     image_caption = models.TextField()
     timestamp = models.DateTimeField(auto_now_add=True)
 
@@ -12,7 +12,7 @@ class ZeeNews(models.Model):
     headline = models.TextField()
     sort_description = models.TextField()
     news = models.TextField()
-    image_source = models.URLField()
+    image_source = models.URLField(max_length=1000)  # Increase max_length if needed
     image_caption = models.TextField()
     timestamp = models.DateTimeField(auto_now_add=True)
 
@@ -20,9 +20,10 @@ class TV9Bangla(models.Model):
     headline = models.TextField()
     sort_description = models.TextField()
     news = models.TextField()
-    image_source = models.URLField()
+    image_source = models.URLField(max_length=1000)  # Increase max_length if needed
     image_caption = models.TextField()
     timestamp = models.DateTimeField(auto_now_add=True)
+
 
 # class Anandabazar(models.Model):
 #     headline = models.TextField()
