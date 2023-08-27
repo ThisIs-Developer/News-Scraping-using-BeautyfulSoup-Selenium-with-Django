ac14c68e3efec5b9772abe386c7908f7e89ea902
Baivab Sarkar <109382325+ThisIs-Developer@users.noreply.github.com>
2023-08-27 14:14:14 +0530

    Update settings.py

--- newspaper_scraper/settings.py
+++ newspaper_scraper/settings.py
@@ -76,8 +76,15 @@ WSGI_APPLICATION = 'newspaper_scraper.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'scrapdata_db',
+        'USER': 'root',
+        'PASSWORD': 'baivab@admin',
+        'HOST': 'localhost',
+        'PORT': '3306',
+        'OPTIONS': {
+            'charset': 'utf8mb4',
+        },
     }
 }
 
