83c0de3b8aae9c8ec1cfc253cb295b6a8b286861
Baivab Sarkar <109382325+ThisIs-Developer@users.noreply.github.com>
2023-08-27 14:13:01 +0530

    Update models.py

--- scrap_app/models.py
+++ scrap_app/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
-# Create your models here.
+class HindustanTimesBangla(models.Model):
+    headline = models.TextField()
+    sort_description = models.TextField()
+    news = models.TextField()
+    image_source = models.URLField()
+    image_caption = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+class ZeeNews(models.Model):
+    headline = models.TextField()
+    sort_description = models.TextField()
+    news = models.TextField()
+    image_source = models.URLField()
+    image_caption = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+class TV9Bangla(models.Model):
+    headline = models.TextField()
+    sort_description = models.TextField()
+    news = models.TextField()
+    image_source = models.URLField()
+    image_caption = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+# class Anandabazar(models.Model):
+#     headline = models.TextField()
+#     sort_description = models.TextField()
+#     news = models.TextField()
+#     image_source = models.URLField()
+#     image_caption = models.TextField()
+#     timestamp = models.DateTimeField(auto_now_add=True)
